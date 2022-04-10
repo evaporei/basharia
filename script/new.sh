ce659e75c86d263c92541dcce752bc5665ca476e
Eva Pace <eba.pachi@gmail.com>
2022-04-10 09:49:57 -0300

    script: Make 'new.sh' accept different shell types

--- script/new.sh
+++ script/new.sh
@@ -1,16 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Usage:
 # ./script/new.sh $SCRIPT_NAME
 SCRIPT_NAME=$1
 
-# 1. Create $SCRIPT_NAME file with shebang
-#
-# TODO: accept different options such as:
-# --sh
-# --mac
-# --fish
-# --zsh
-echo '#!/bin/bash' > $SCRIPT_NAME
+SHEBANG_PREFIX='#!/usr/bin/env'
+
+# Set default shell
+SHELL='bash'
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --bash)
+      SHELL='bash'
+      shift
+      ;;
+    --sh)
+      SHELL='sh'
+      shift
+      ;;
+    --zsh)
+      SHELL='zsh'
+      shift
+      ;;
+    --fish)
+      SHELL='fish'
+      shift
+      ;;
+    --*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+SCRIPT_NAME=$POSITIONAL_ARGS
+
+# 1. Create $SCRIPT_NAME file with shebang and chosen shell
+echo "$SHEBANG_PREFIX $SHELL" > $SCRIPT_NAME
 
 # 2. Make it an executable
 chmod +x $SCRIPT_NAME
